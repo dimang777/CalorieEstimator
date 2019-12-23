87e8d8c318a9c03eb2675c1a10e097c80720298b
Isaac Sung Jae Chang <41130338+dimang777@users.noreply.github.com>
2019-12-22 21:32:52 -0500

    xgb complete

--- scripts/model/xgb.py
+++ scripts/model/xgb.py
@@ -5,6 +5,7 @@ import numpy as np
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.model_selection import GridSearchCV
 import xgboost
+import matplotlib.pyplot as plt
 
 ###############################################################################
 # Set up folders and variables
@@ -36,19 +37,25 @@ with open(save_folder + 'train_test_sel_features.pkl', 'rb') as f:
 # Set up hyperparameters
 ###############################################################################
 
-
+n_estimators = np.arange(100, 1000, 100)
 max_depth = range(3, 11)
 learning_rate = np.arange(0.01, 0.1+0.01, 0.01)
 subsample = np.arange(0.8, 1+0.04, 0.04)
 colsample_bytree = np.arange(0.4, 1+0.1, 0.1)
 gamma = [0, 1, 5]
+early_stopping_rounds = np.arange(5, 25, 5)
 eval_metric = ['merror']
-random_grid = {'max_depth': max_depth,
+eval_set_plot = [(x_train_sel_df.values, y_train_sel_df.values)]
+
+random_grid = {'n_estimators': n_estimators,
+               'max_depth': max_depth,
                'learning_rate': learning_rate,
                'subsample': subsample,
                'colsample_bytree': colsample_bytree,
                'gamma': gamma,
-               'eval_metric': eval_metric}
+               'early_stopping_rounds': early_stopping_rounds,
+               'eval_metric': eval_metric,
+               'eval_set_plot': eval_set_plot}
 print(random_grid)
 
 ###############################################################################
@@ -71,7 +78,8 @@ def evaluate(model, test_features, test_labels):
 
 
 # Use the random grid to search for best hyperparameters
-# add tree_method = 'gpu_hist' for GPU
+# add tree_method = 'gpu_hist' for GPU - but doesn't seem to be 
+# compatible with CV and multicore deployment
 model = xgboost.XGBClassifier(objective='multi:softprob',
                               num_class=3)
 
@@ -91,18 +99,10 @@ xgb_random.fit(x_train_sel_df.values, y_train_sel_df.values)
 print(xgb_random.best_params_)
 end = time.time()
 print(end - start)
-#!@@$!#@!#!$QTQ$%@#%!@#!$#@$!#@&^*%(&^)&* WORKED UNTIL HERE
-#!@@$!#@!#!$QTQ$%@#%!@#!$#@$!#@&^*%(&^)&* WORKED UNTIL HERE
-#!@@$!#@!#!$QTQ$%@#%!@#!$#@$!#@&^*%(&^)&* WORKED UNTIL HERE
-#!@@$!#@!#!$QTQ$%@#%!@#!$#@$!#@&^*%(&^)&* WORKED UNTIL HERE
-#!@@$!#@!#!$QTQ$%@#%!@#!$#@$!#@&^*%(&^)&* WORKED UNTIL HERE
-#!@@$!#@!#!$QTQ$%@#%!@#!$#@$!#@&^*%(&^)&* WORKED UNTIL HERE
-#!@@$!#@!#!$QTQ$%@#%!@#!$#@$!#@&^*%(&^)&* WORKED UNTIL HERE
-#!@@$!#@!#!$QTQ$%@#%!@#!$#@$!#@&^*%(&^)&* WORKED UNTIL HERE
 
-# 48 tasks
-# Using multicore - 67.8s
-# Using single core - 3.5s
+# _ tasks
+# Using multicore - _s
+# Using single core - _s
 
 model = xgb_random.best_estimator_
 random_accuracy_train = evaluate(model,
@@ -117,15 +117,30 @@ random_accuracy_test = evaluate(model,
 ###############################################################################
 
 # Create the parameter grid based on the results of random search
-C = [int(x) for x in np.linspace(start=900, stop=1100, num=21)]
-kernel = ['linear']
-gamma = [(x) for x in np.linspace(start=0.0002, stop=0.002, num=10)]
-param_grid = {'C': C,
-              'kernel': kernel,
-              'gamma': gamma}
+n_estimators = np.arange(880, 920, 10)
+max_depth = [4]
+learning_rate = np.arange(0.048, 0.052+0.001, 0.001)
+subsample = np.arange(0.82, 0.86+0.01, 0.01)
+colsample_bytree = np.arange(0.68, 0.72+0.01, 0.01)
+gamma = [1]
+early_stopping_rounds = [20]
+eval_metric = ['merror']
+eval_set_plot = [(x_train_sel_df.values, y_train_sel_df.values)]
+
+param_grid = {'n_estimators': n_estimators,
+               'max_depth': max_depth,
+               'learning_rate': learning_rate,
+               'subsample': subsample,
+               'colsample_bytree': colsample_bytree,
+               'gamma': gamma,
+               'early_stopping_rounds': early_stopping_rounds,
+               'eval_metric': eval_metric,
+               'eval_set_plot': eval_set_plot}
 print(param_grid)
 
-model = svm.SVC()
+
+model = xgboost.XGBClassifier(objective='multi:softprob',
+                              num_class=3)
 start = time.time()
 # Instantiate the grid search model
 grid_search = GridSearchCV(estimator=model,
@@ -140,9 +155,9 @@ print(grid_search.best_params_)
 # {'C': 1040, 'gamma': 0.0002, 'kernel': 'linear'}
 end = time.time()
 print(end - start)
-# 630 tasks
-# Using multicore - 7.5s
-# Using single core - 9.93s
+# 1800 tasks
+# Using multicore - ~2 hours
+# Using single core - _s
 
 ###############################################################################
 # Final model
@@ -157,18 +172,18 @@ grid_accuracy_test = evaluate(model,
                               y_test_sel_df.values)
 
 # =============================================================================
-# Accuracy Test: 98.88%
-# Accuracy Test: 98.51%
+# Accuracy Test: 99.88%
+# Accuracy Test: 95.29%
 # =============================================================================
 
 ###############################################################################
 # Save model
 ###############################################################################
 
-with open(model_folder+'svm_hyper_tune_bestmodel_sel_fea.pickle', 'wb') \
+with open(model_folder+'xgb_hyper_tune_bestmodel_sel_fea.pickle', 'wb') \
   as handle:
     pickle.dump([model], handle)
 
-with open(model_folder+'svm_hyper_tune_bestmodel_sel_fea.pickle', 'rb') \
+with open(model_folder+'xgb_hyper_tune_bestmodel_sel_fea.pickle', 'rb') \
   as handle:
     [model] = pickle.load(handle)
